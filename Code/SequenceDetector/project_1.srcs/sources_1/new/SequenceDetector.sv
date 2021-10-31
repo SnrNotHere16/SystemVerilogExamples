8fb2dcedb1536829636cc2b3ea8de95b51c2cd08
SnrNotHere16 <aquineda16@hotmail.com>
2021-10-30 22:30:00 -0700

    Setup the Procedual blocks for the Sequence Detector

--- Code/SequenceDetector/project_1.srcs/sources_1/new/SequenceDetector.sv
+++ Code/SequenceDetector/project_1.srcs/sources_1/new/SequenceDetector.sv
@@ -26,5 +26,29 @@ module SequenceDetector(input wire logic clk,
                         input wire logic M, 
                         output var logic Z, 
                         output var logic [2:0] Q);
-
+    //Enum for Moore states              
+    typedef enum logic [2:0] {RESET, Next} moore_states_t; 
+    moore_states_t current_state, next_state; 
+                    
+    //Current State logic -- Sequential   
+    always_ff @(posedge clk, posedge reset) begin 
+        if (reset) begin 
+            current_state <= RESET; 
+        end 
+        
+        else begin 
+            current_state <= next_state; 
+        end  
+    end 
+    //Next State Decoder -- Combinational 
+    always_comb begin 
+    
+    
+    end 
+    
+    //Output Decoder -- Combinational 
+    always_comb begin 
+    
+    
+    end 
 endmodule
