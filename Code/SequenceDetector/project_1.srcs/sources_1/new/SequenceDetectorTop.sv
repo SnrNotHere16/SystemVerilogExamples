4bf4c39f3f1f56f0fa9f1adaf3d4585bf61a9554
SnrNotHere16 <aquineda16@hotmail.com>
2021-10-31 19:07:56 -0700

    Added naming star infer

--- Code/SequenceDetector/project_1.srcs/sources_1/new/SequenceDetectorTop.sv
+++ Code/SequenceDetector/project_1.srcs/sources_1/new/SequenceDetectorTop.sv
@@ -20,6 +20,6 @@ module SequenceDetectorTop(
      wire clk_out, one_out; 
      Clk500Hz zero(.*);//dot star inferred name port connection
      OneShot one (.d_in(step),.clk_in(clk_out),.reset(reset),.d_out(one_out));
-    SequenceDetector two(.clk(one_out),.reset(reset), .X(X), .Z(Z), .Q(Q)); 
+    SequenceDetector two(.clk(one_out),.reset(reset), .*); 
                             
 endmodule
