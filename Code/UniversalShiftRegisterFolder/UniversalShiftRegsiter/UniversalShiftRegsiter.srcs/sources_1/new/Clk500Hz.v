035da20bc4f4f241d96aaa70af180fbe7b2340b8
SnrNotHere16 <aquineda16@hotmail.com>
2021-11-24 12:13:47 -0800

    Added comments

---
+++ Code/UniversalShiftRegisterFolder/UniversalShiftRegsiter/UniversalShiftRegsiter.srcs/sources_1/new/Clk500Hz.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/21/2021 06:25:45 AM
+// Design Name: 
+// Module Name: Clk500Hz
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Clk500Hz(
+
+    );
+endmodule
