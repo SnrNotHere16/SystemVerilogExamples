10ab71d751058a5f403525273bf515284443cea9
SnrNotHere16 <aquineda16@hotmail.com>
2021-11-25 15:59:46 -0800

    Added Tesetbench values

--- Code/SequenceDetectorMealy/SequenceDetectorMealy.srcs/sim_1/new/SequenceDetectorTB.sv
+++ Code/SequenceDetectorMealy/SequenceDetectorMealy.srcs/sim_1/new/SequenceDetectorTB.sv
@@ -1,26 +1,60 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/25/2021 11:14:09 AM
-// Design Name: 
-// Module Name: SequenceDetectorTB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module SequenceDetectorTB(
+module SequenceDetectorTB();
+        logic clk;
+        logic reset;
+        logic step; 
+        logic X; 
+        wire Z; 
+        wire [2:0] Q;
+        const logic [4:0] seq = 5'b010110; 
+        task apply_reset();
+            repeat (5) @(posedge clk) begin 
+                reset <= 1'b1;
+                step <= 1'b0; 
+                X <= 1'b0; 
+            end 
+            reset <= 1'b0; 
+        endtask 
+        task send_full_sequence(); 
+//            @ (posedge clk) begin 
+//                step <= 1'b1; 
+//            end 
+//            for (int i = 0; i < 5; i++) begin 
+//                repeat (20000) @(posedge clk) begin 
+//                    step <= 1'b1; 
+//                    X <= 1'b0;                 
+//                end 
+//            end 
+                   repeat (1800000) @(posedge clk) begin 
+                    step <= 1'b1; 
+                    X <= 1'b0;
+                    end  
+                   repeat (207500) @(posedge clk) begin 
+                    step <= 1'b0; 
+                    X <= seq[0];
+                    end  
+                    repeat (1800000) @(posedge clk) begin 
+                    step <= 1'b1; 
+                    X <= seq[0];
+                    end  
+                    @ (posedge clk) X <= 1'b1; 
+                    
+        endtask 
+        
+        SequenceDetectorTop DUT(.*); 
+        initial begin //initialize clock 
+            clk <= 1'b0;  
+        end 
+        always #5 clk = ~clk; //10ns period 
 
-    );
+        initial begin 
+            apply_reset();
+            send_full_sequence();  
+            step <= 1'b0; 
+        
+          $finish; 
+        end  
+        
 endmodule
