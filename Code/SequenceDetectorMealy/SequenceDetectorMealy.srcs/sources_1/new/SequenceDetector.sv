aefe457c4bd1502aedbac5afc7c2d04947bc7dde
SnrNotHere16 <aquineda16@hotmail.com>
2021-11-24 21:11:04 -0800

    Completed Mealy FSM and implemented on FPGA

--- Code/SequenceDetectorMealy/SequenceDetectorMealy.srcs/sources_1/new/SequenceDetector.sv
+++ Code/SequenceDetectorMealy/SequenceDetectorMealy.srcs/sources_1/new/SequenceDetector.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 //Filename: SequenceDetector.sv 
-// Provides logic for a sequnce detector(non overlapping) via a MOORE FSM 
+// Provides logic for a sequnce detector(non overlapping) via a MEALY FSM 
 // Detects sequence 010110
 //X - input 
 //Z - Ouput: Z ==1 if sequnece detected 
@@ -17,9 +17,46 @@
 module SequenceDetector( input wire logic clk, 
                          input wire logic reset, 
                          input wire  logic X, 
-                         output var logic Y,
-                         output var logic [2:0] Z);
+                         output var logic Z,
+                         output var logic [2:0] Q);
                          
       typedef enum logic [2:0] {RESET, ZERO, ONE, TWO, THREE, FOUR, FIVE} mealy_states_t;
       mealy_states_t current_state, next_state; 
+        //Current State logic -- Sequential   
+    always_ff @(posedge clk, posedge reset) begin 
+        if (reset) begin 
+            current_state <= current_state.first(); 
+        end 
+        
+        else begin 
+            current_state <= next_state; 
+        end  
+    end 
+    //Next State Decoder -- Combinational 
+     always_comb begin 
+       unique case(current_state)
+            RESET: next_state = ZERO;
+            ZERO: next_state=(X)? ZERO: ONE; 
+            ONE: next_state=(X)? TWO: ONE;
+            TWO: next_state=(X)? ZERO: THREE; 
+            THREE: next_state=(X)? FOUR: ONE; 
+            FOUR: next_state=(X)? FIVE: THREE; 
+            FIVE: next_state=(X)? ZERO: ONE; 
+            default:  next_state = ZERO; 
+         endcase
+    end
+    
+    //Output Decoder -- Combinational 
+    always_comb begin 
+        unique case (current_state) 
+            RESET: {Q, Z} = 4'b111_0; 
+            ZERO: {Q, Z} = 4'b000_0; 
+            ONE: {Q, Z} = 4'b001_0; 
+            TWO: {Q, Z} = 4'b010_0; 
+            THREE: {Q, Z} = 4'b011_0;
+            FOUR: {Q, Z} = 4'b100_0;  
+            FIVE: {Q, Z} = (X)? 4'b101_0: 4'b101_1; 
+            default: {Q,Z} = 4'b000_0; 
+          endcase  
+      end
 endmodule
