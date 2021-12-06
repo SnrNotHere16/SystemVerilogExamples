cca31bb11586daf8aebf9dd1ea4c19a3e7aeb65c
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-05 21:23:04 -0800

    Testbench completed

--- Code/SequenceDetectorInterface/project_1.sim/sim_1/behav/xsim/xsim.dir/SequenceDetectorInterfaceTB_behav/obj/xsim_1.c
+++ Code/SequenceDetectorInterface/project_1.sim/sim_1/behav/xsim/xsim.dir/SequenceDetectorInterfaceTB_behav/obj/xsim_1.c
@@ -46,41 +46,43 @@ typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 extern void execute_15(char*, char *);
 extern void execute_16(char*, char *);
-extern void execute_17(char*, char *);
+extern void execute_23(char*, char *);
+extern void execute_36(char*, char *);
+extern void execute_37(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_39(char*, char *);
+extern void execute_40(char*, char *);
+extern void execute_41(char*, char *);
+extern void execute_29(char*, char *);
 extern void execute_30(char*, char *);
 extern void execute_31(char*, char *);
 extern void execute_32(char*, char *);
 extern void execute_33(char*, char *);
 extern void execute_34(char*, char *);
 extern void execute_35(char*, char *);
-extern void execute_23(char*, char *);
-extern void execute_24(char*, char *);
-extern void execute_25(char*, char *);
-extern void execute_26(char*, char *);
-extern void execute_27(char*, char *);
-extern void execute_28(char*, char *);
-extern void execute_29(char*, char *);
 extern void execute_8(char*, char *);
 extern void execute_10(char*, char *);
-extern void execute_22(char*, char *);
+extern void execute_28(char*, char *);
 extern void execute_12(char*, char *);
 extern void execute_13(char*, char *);
 extern void execute_14(char*, char *);
-extern void execute_19(char*, char *);
-extern void execute_20(char*, char *);
-extern void execute_21(char*, char *);
-extern void execute_36(char*, char *);
-extern void execute_37(char*, char *);
-extern void execute_38(char*, char *);
-extern void execute_39(char*, char *);
-extern void execute_40(char*, char *);
+extern void execute_25(char*, char *);
+extern void execute_26(char*, char *);
+extern void execute_27(char*, char *);
+extern void execute_42(char*, char *);
+extern void execute_43(char*, char *);
+extern void execute_44(char*, char *);
+extern void execute_45(char*, char *);
+extern void execute_46(char*, char *);
 extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[31] = {(funcp)execute_15, (funcp)execute_16, (funcp)execute_17, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)execute_34, (funcp)execute_35, (funcp)execute_23, (funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_28, (funcp)execute_29, (funcp)execute_8, (funcp)execute_10, (funcp)execute_22, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_19, (funcp)execute_20, (funcp)execute_21, (funcp)execute_36, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 31;
+extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
+extern void vlog_transfunc_eventcallback_2state(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[33] = {(funcp)execute_15, (funcp)execute_16, (funcp)execute_23, (funcp)execute_36, (funcp)execute_37, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)execute_41, (funcp)execute_29, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)execute_34, (funcp)execute_35, (funcp)execute_8, (funcp)execute_10, (funcp)execute_28, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_42, (funcp)execute_43, (funcp)execute_44, (funcp)execute_45, (funcp)execute_46, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_0, (funcp)vlog_transfunc_eventcallback_2state};
+const int NumRelocateId= 33;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/SequenceDetectorInterfaceTB_behav/xsim.reloc",  (void **)funcTab, 31);
+	iki_relocate(dp, "xsim.dir/SequenceDetectorInterfaceTB_behav/xsim.reloc",  (void **)funcTab, 33);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
--- Code/SequenceDetectorInterface/project_1.srcs/sim_1/new/SequenceDetectorInterfaceTB.sv
+++ Code/SequenceDetectorInterface/project_1.srcs/sim_1/new/SequenceDetectorInterfaceTB.sv
@@ -1,22 +1,8 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/05/2021 08:38:12 PM
-// Design Name: 
-// Module Name: SequenceDetectorInterfaceTB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// File: SequenceDetectorInterfaceTB.sv
+// The following test bench tests for the sequence 010110. 
+// The LSB is the right most bit. Right to left. 
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -26,26 +12,65 @@ module SequenceDetectorInterfaceTB();
     wire [2:0] Q;
     parameter step_on = 1800000;
     parameter step_off = 200001;
+    const logic [0:5] seq = 6'b0101_10;
+    const logic [5:0] seq_rev = 6'b0101_10; 
     
     SequenceDetectorTop DUT(.*); 
-    
+   
     initial begin 
         clk = 1'b0; 
     end 
     always#5 clk = ~clk; 
-    
-    initial begin 
+    task init(); 
         repeat (5) @(posedge clk) reset <= 1'b1; 
-        repeat (2) @ (posedge clk) reset <= 1'b0; 
-        repeat (1) @ (posedge clk)  begin 
-            step <= 1'b0;
-            X <= 1'b0;  
+        repeat (5) @(posedge clk) begin 
+            reset <= 1'b0 ;
+            step <= 1'b0; 
+            X <= 1'b0; 
         end 
-        repeat (step_on)@ (negedge clk) step <= 1'b1; 
-        repeat (step_off) @ (negedge clk) step <= 1'b0; 
-        repeat (step_on) @ (negedge clk) step <= 1'b1; 
-        
-        
-     $finish;    
+    endtask 
+    
+    task sendbit(input logic in); 
+      @(posedge clk)  begin X <= in; step <= 1'b1; end 
+      repeat (step_on) @ (negedge clk) begin 
+        X <= in; 
+      end 
+      repeat (step_off) @ (negedge clk) begin 
+        step <= 1'b0; 
+      end 
+    endtask
+    
+    task send_sequence(); 
+        for (int i = 0; i < 6; i++) begin 
+             repeat (step_on) @ (negedge clk) begin 
+                step <= 1'b1; 
+                X <= seq[i]; 
+             end 
+             repeat (step_off) @ (negedge clk) begin 
+                step <= 1'b0; 
+             end 
+         end 
+    endtask
+    
+    task send_sequence_rev(); 
+        for (int i = 0 ; i < 6; i++) begin 
+           repeat (step_on) @ (negedge clk) begin 
+                step <= 1'b1; 
+                X <= seq_rev[i]; 
+             end 
+             repeat (step_off) @ (negedge clk) begin 
+                step <= 1'b0; 
+             end
+        end
+    
+    endtask
+    initial begin 
+        $display("Begin");
+        init(); 
+        sendbit(1'b1); 
+        send_sequence_rev(); 
+        send_sequence(); 
+        $display("Finished");  
+        $finish;    
     end 
 endmodule
