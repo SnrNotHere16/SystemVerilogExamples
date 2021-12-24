c53abd5f5d2eb713d2b4b8ca0bb17213d5e66a39
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-24 13:31:03 -0800

    update coverage

--- Code/FunctionalCoverageTB6.sv
+++ Code/FunctionalCoverageTB6.sv
@@ -3,21 +3,44 @@ module FunctionalCoverageTB6 ();
 	logic [1:0] a,b; 
 	logic [2:0] c,d; 
 	logic [3:0] e,f; 
+	typedef enum logic [1:0] {ZERO, ONE, TWO, THREE} num; 
+	typedef enum logic [2:0] {A, B, C, D, E, F, G, H} letter; 
 	
-	covergroup a_c; 
+  	num numi; 
+  	letter letteri; 
+  	covergroup a_c @(posedge clk); 
 		a1: coverpoint a; 
 		c1: coverpoint c; 
 		aXc: cross a,c; 
 	endgroup: a_c
   
+  	covergroup enumgroup @(posedge clk); 
+    	num1: coverpoint numi; 
+      	letter1: coverpoint letteri; 
+    endgroup: enumgroup
+  
 	initial begin 
 		clk = 1'b0; 
 	end 
+  
 	always #5 clk = ~clk; 
 	
 	a_c a_c_i = new(); 
+	enumgroup ei = new(); 
 	initial begin 
       $display("Begin"); 
+      @(posedge clk) begin 
+       	a <= 0; 
+        c <= 0; 
+      end       
+      @(posedge clk) begin 
+       	a <= 0; 
+        c <= 1; 
+      end 
+      @(posedge clk) begin 
+       	a <= 0; 
+        c <= 0; 
+      end 
       $stop; 
     end 
 
