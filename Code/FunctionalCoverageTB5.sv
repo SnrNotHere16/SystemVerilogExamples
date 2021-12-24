633f0e240c83c99c6337d259bb2ddce44dece788
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-23 23:36:53 -0800

    Update FunctionalCoverageTB5.sv
    Finished FunctionalCoverageTB5.sv

--- Code/FunctionalCoverageTB5.sv
+++ Code/FunctionalCoverageTB5.sv
@@ -1,3 +1,183 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// File: FunctionalCoverageTB5.sv
+// output: 
+
+//# do run.do
+//# Begin
+//# ** Error: (vsim-8565) Illegal state bin was hit at value=4. The bin counter for the illegal bin '\/FunctionalCoverageTB5/a_c_i .c1.c2' is 1.
+//#    Time: 405 ns  Iteration: 0  Region: /FunctionalCoverageTB5/a_c_c
+//# ** Note: $stop    : testbench.sv(104)
+//#    Time: 405 ns  Iteration: 1  Instance: /FunctionalCoverageTB5
+//# Break at testbench.sv line 104
+//# Coverage Report by instance with details
+//# 
+//# =================================================================================
+//# === Instance: /FunctionalCoverageTB5
+//# === Design Unit: work.FunctionalCoverageTB5
+//# =================================================================================
+//# 
+//# Covergroup Coverage:
+//#     Covergroups                      2        na        na    75.00%
+//#         Coverpoints/Crosses          4        na        na        na
+//#             Covergroup Bins         17        15         2    88.23%
+//# ----------------------------------------------------------------------------------------------------------
+//# Covergroup                                             Metric       Goal       Bins    Status               
+//#                                                                                                          
+//# ----------------------------------------------------------------------------------------------------------
+//#  TYPE /FunctionalCoverageTB5/a_c_c                    100.00%        100          -    Covered              
+//#     covered/total bins:                                     8          8          -                      
+//#     missing/total bins:                                     0          8          -                      
+//#     % Hit:                                            100.00%        100          -                      
+//#     Coverpoint a1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#     Coverpoint c1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 1          1          -                      
+//#         missing/total bins:                                 0          1          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#  Covergroup instance \/FunctionalCoverageTB5/a_c_i  
+//#                                                       100.00%        100          -    Covered              
+//#     covered/total bins:                                     8          8          -                      
+//#     missing/total bins:                                     0          8          -                      
+//#     % Hit:                                            100.00%        100          -                      
+//#     Coverpoint a1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         bin a0                                              1          1          -    Covered              
+//#         bin a1[0]                                           1          1          -    Covered              
+//#         bin a1[1]                                           1          1          -    Covered              
+//#         bin a1[2]                                           1          1          -    Covered              
+//#         bin a1[3]                                           1          1          -    Covered              
+//#         bin a2[0]                                           2          1          -    Covered              
+//#         bin a2[1]                                          33          1          -    Covered              
+//#     Coverpoint c1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 1          1          -                      
+//#         missing/total bins:                                 0          1          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         illegal_bin c2                                      1                     -    Occurred             
+//#         ignore_bin c1                                       1                     -    Occurred             
+//#         bin c0                                              1          1          -    Covered              
+//#  TYPE /FunctionalCoverageTB5/b_d_b                     50.00%        100          -    Uncovered            
+//#     covered/total bins:                                     7          9          -                      
+//#     missing/total bins:                                     2          9          -                      
+//#     % Hit:                                             77.77%        100          -                      
+//#     Coverpoint b1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#     Coverpoint d1                                       0.00%        100          -    ZERO                 
+//#         covered/total bins:                                 0          2          -                      
+//#         missing/total bins:                                 2          2          -                      
+//#         % Hit:                                          0.00%        100          -                      
+//#  Covergroup instance \/FunctionalCoverageTB5/b_d_i  
+//#                                                        50.00%        100          -    Uncovered            
+//#     covered/total bins:                                     7          9          -                      
+//#     missing/total bins:                                     2          9          -                      
+//#     % Hit:                                             77.77%        100          -                      
+//#     Coverpoint b1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         bin b0                                              2          1          -    Covered              
+//#         bin b1                                              1          1          -    Covered              
+//#         bin b2                                              2          1          -    Covered              
+//#         bin b3[1=>3]                                        1          1          -    Covered              
+//#         bin b3[1=>4]                                        1          1          -    Covered              
+//#         bin b3[1=>5]                                        1          1          -    Covered              
+//#         bin b4                                              4          1          -    Covered              
+//#     Coverpoint d1                                       0.00%        100          -    ZERO                 
+//#         covered/total bins:                                 0          2          -                      
+//#         missing/total bins:                                 2          2          -                      
+//#         % Hit:                                          0.00%        100          -                      
+//#         bin d0[0=>1]                                        0          1          -    ZERO                 
+//#         bin d0[1=>2]                                        0          1          -    ZERO                 
+//# 
+//# COVERGROUP COVERAGE:
+//# ----------------------------------------------------------------------------------------------------------
+//# Covergroup                                             Metric       Goal       Bins    Status               
+//#                                                                                                          
+//# ----------------------------------------------------------------------------------------------------------
+//#  TYPE /FunctionalCoverageTB5/a_c_c                    100.00%        100          -    Covered              
+//#     covered/total bins:                                     8          8          -                      
+//#     missing/total bins:                                     0          8          -                      
+//#     % Hit:                                            100.00%        100          -                      
+//#     Coverpoint a1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#     Coverpoint c1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 1          1          -                      
+//#         missing/total bins:                                 0          1          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#  Covergroup instance \/FunctionalCoverageTB5/a_c_i  
+//#                                                       100.00%        100          -    Covered              
+//#     covered/total bins:                                     8          8          -                      
+//#     missing/total bins:                                     0          8          -                      
+//#     % Hit:                                            100.00%        100          -                      
+//#     Coverpoint a1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         bin a0                                              1          1          -    Covered              
+//#         bin a1[0]                                           1          1          -    Covered              
+//#         bin a1[1]                                           1          1          -    Covered              
+//#         bin a1[2]                                           1          1          -    Covered              
+//#         bin a1[3]                                           1          1          -    Covered              
+//#         bin a2[0]                                           2          1          -    Covered              
+//#         bin a2[1]                                          33          1          -    Covered              
+//#     Coverpoint c1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 1          1          -                      
+//#         missing/total bins:                                 0          1          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         illegal_bin c2                                      1                     -    Occurred             
+//#         ignore_bin c1                                       1                     -    Occurred             
+//#         bin c0                                              1          1          -    Covered              
+//#  TYPE /FunctionalCoverageTB5/b_d_b                     50.00%        100          -    Uncovered            
+//#     covered/total bins:                                     7          9          -                      
+//#     missing/total bins:                                     2          9          -                      
+//#     % Hit:                                             77.77%        100          -                      
+//#     Coverpoint b1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#     Coverpoint d1                                       0.00%        100          -    ZERO                 
+//#         covered/total bins:                                 0          2          -                      
+//#         missing/total bins:                                 2          2          -                      
+//#         % Hit:                                          0.00%        100          -                      
+//#  Covergroup instance \/FunctionalCoverageTB5/b_d_i  
+//#                                                        50.00%        100          -    Uncovered            
+//#     covered/total bins:                                     7          9          -                      
+//#     missing/total bins:                                     2          9          -                      
+//#     % Hit:                                             77.77%        100          -                      
+//#     Coverpoint b1                                     100.00%        100          -    Covered              
+//#         covered/total bins:                                 7          7          -                      
+//#         missing/total bins:                                 0          7          -                      
+//#         % Hit:                                        100.00%        100          -                      
+//#         bin b0                                              2          1          -    Covered              
+//#         bin b1                                              1          1          -    Covered              
+//#         bin b2                                              2          1          -    Covered              
+//#         bin b3[1=>3]                                        1          1          -    Covered              
+//#         bin b3[1=>4]                                        1          1          -    Covered              
+//#         bin b3[1=>5]                                        1          1          -    Covered              
+//#         bin b4                                              4          1          -    Covered              
+//#     Coverpoint d1                                       0.00%        100          -    ZERO                 
+//#         covered/total bins:                                 0          2          -                      
+//#         missing/total bins:                                 2          2          -                      
+//#         % Hit:                                          0.00%        100          -                      
+//#         bin d0[0=>1]                                        0          1          -    ZERO                 
+//#         bin d0[1=>2]                                        0          1          -    ZERO                 
+//# 
+//# TOTAL COVERGROUP COVERAGE: 75.00%  COVERGROUP TYPES: 2
+//# 
+//# Total Coverage By Instance (filtered view): 75.00%
+//# 
+//# 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module FunctionalCoverageTB5();
 	logic clk; 
 	logic [3:0] a,b; 
@@ -26,7 +206,11 @@ module FunctionalCoverageTB5();
           bins b1 = (0 => 1 => 2); // 1 bin 
           bins b2 = (0 =>2),(2 => 1); // 1 bin or 
           bins b3 [] = (1 => 3), (1 =>4), (1=>5); // 3 bins 
-          bins b4 = (6,7 => 8,9);//1 bin, 4 combinations } 
+          bins b4 = (6,7 => 8,9);//1 bin, 4 combinations 
+          }
+        d1: coverpoint d {
+          bins d0 [] = (0 => 1), (1 => 2);   	 
+          }
     endgroup: b_d_b
 
 	
@@ -45,7 +229,7 @@ module FunctionalCoverageTB5();
     endtask: a_bins 
 	
 	task c_bins; 
-      ``@(posedge clk) c <= 1'b0; 
+        @(posedge clk) c <= 1'b0; 
       	@(posedge clk) c <= c+1; 
       	@(posedge clk) c <= c+3; 
       	@(posedge clk) c <= 1'b0;
