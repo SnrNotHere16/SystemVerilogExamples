a67352b63a4b2661499ac7e6d8c05d3c783c2d7c
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-23 23:11:42 -0800

    Update FunctionalCoverageTB5.sv

--- Code/FunctionalCoverageTB5.sv
+++ Code/FunctionalCoverageTB5.sv
@@ -12,7 +12,23 @@ module FunctionalCoverageTB5();
           	bins a1[4] = {5,[6:9],10}; // a1 4 bins: (5, 6, 7, [8:10])
             bins a2 [2] = {11, [12:15]}; 
 		}
+		
+		c1: coverpoint c {
+			bins c0 = {0}; 
+			ignore_bins c1 = {1, 2, 3};
+			illegal_bins c2 = {4, 5, 6};
+		}
 	endgroup: a_c_c
+  	
+  	covergroup b_d_b @(posedge clk); 
+      	b1: coverpoint b {
+          bins b0 = (0 => 1);
+          bins b1 = (0 => 1 => 2);
+          bins b2 = (0 =>2),(2 => 1);
+          bins b3 [] = (1 => 3), (1 =>4), (1=>5);}
+    endgroup: b_d_b
+
+	
   	task a_bins; 
       @(posedge clk) a <= 1; //bins a0 
       @(posedge clk) a <= 5; //bins a1[0]
@@ -26,13 +42,50 @@ module FunctionalCoverageTB5();
       @(posedge clk) a <= 15;//bins a2[1] 
       @(posedge clk) a <= 14;//bins a2[1]
     endtask: a_bins 
+	
+	task c_bins; 
+      ``@(posedge clk) c <= 1'b0; 
+      	@(posedge clk) c <= c+1; 
+      	@(posedge clk) c <= c+3; 
+      	@(posedge clk) c <= 1'b0;
+	endtask: c_bins 
+  
+  	task b_bins; 
+      //bin b0 
+      @(posedge clk) b <= 1'b0; 
+      @(posedge clk) b <= b+1;
+      @(posedge clk) b <= b; 
+      //bin b1 
+      @(posedge clk) b <= 1'b0; 
+      @(posedge clk) b <= b+1;
+      @(posedge clk) b <= b+1;
+      @(posedge clk) b <= b;
+      //bin b2 
+      @(posedge clk) b <= 0; 
+      @(posedge clk) b <= 2; 
+      @(posedge clk) b <= b;
+      //@(posedge clk) b <= 1; 
+      //@(posedge clk) b <= b; 
+      //bin b3 
+      @(posedge clk) b <= 1; 
+      @(posedge clk) b <= 3; 
+      @(posedge clk) b <= 1; 
+      @(posedge clk) b <= 4;
+      @(posedge clk) b <= 1; 
+      @(posedge clk) b <= 5;
+      @(posedge clk) b <= b; 
+      
+    endtask: b_bins 
   
   	always #5 clk = ~clk; 
   
     a_c_c a_c_i = new(); 
+	b_d_b b_d_i = new(); 
 	initial begin 
 		$display("Begin"); 
       	a_bins; 
+      	b_bins; 
+		c_bins; 
       	$stop; 
 	end 
 endmodule: FunctionalCoverageTB5
