d6576b8223e81a75f0e44ea1e31cb8937c269e06
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-25 21:35:39 -0800

    Update FunctionalCoverageTB7.sv

--- Code/FunctionalCoverageTB7.sv
+++ Code/FunctionalCoverageTB7.sv
@@ -1,18 +1,19 @@
 module FunctionalCoverageTB7(); 
 	logic clk; 
+  	logic rest; 
 	class coverPClass; 
 		logic [3:0] a,b; 
 		logic [2:0] c; 
+		covergroup a_c @(posedge clk);  
+        	a1: coverpoint a; 
+          	c1: coverpoint c; 
+          	//aXc: cross a,c; 
+        endgroup: a_c
 		
 		function new(); 
 			$display("coverPClass created\n");
- 
+			a_c = new(); 
 		endfunction: new 
-      	covergroup a_c @(posedge clk);  
-        	a1: coverpoint a; 
-          	c1: coverpoint c; 
-          	aXc: cross a,c; 
-        endgroup: a_c
 	endclass: coverPClass 
 	
   	initial begin 
@@ -21,7 +22,7 @@ module FunctionalCoverageTB7();
 	always #5 clk = ~clk; 
   	coverPClass cov = new(); 
 	initial begin 
-      $display("Begin");
+      $display("Begin");  
       $stop; 
     end 
   
