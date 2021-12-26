ba2d356e4c20f2f3918205e2278b13f98aacca85
SnrNotHere16 <aquineda16@hotmail.com>
2021-12-25 22:30:44 -0800

    Update FunctionalCoverageTB7.sv

--- Code/FunctionalCoverageTB7.sv
+++ Code/FunctionalCoverageTB7.sv
@@ -7,8 +7,13 @@ module FunctionalCoverageTB7();
 		logic [2:0] c; 
 		covergroup a_c @(posedge clk);  
           a1: coverpoint a {
-            bins a1 [] = {[0:10]} with (item%3 == 0); } //divides the bins into only values that fufill the statement
-          	c1: coverpoint c; 
+            bins a0 [] = {[0:10]} with (item%3 == 0); //divides the bins into only values that fufill the statement, 4 bins
+            bins at0 = (3[*3]); // 3=>3=>3=>3 
+            bins at1 = (4[*3:5]); //( 3=>3=>3 ), ( 3=>3=>3=>3 ), ( 3=>3=>3=>3=>3 )
+            bins at2 = (5 [= 2]);//
+            bins at3 = (4 => 5 => 6), ([7:9],10=>11,12); // 4=>5=>6, or 7=>11, 8=>11, 9=>11, 10=>11, 7=>12, 8=>12, 9=>12, 10=>12.
+			} 
+          c1: coverpoint c; 
           	//aXc: cross a,c; 
         endgroup: a_c
 		
@@ -30,6 +35,10 @@ module FunctionalCoverageTB7();
   	coverPClass cov = new(); 
 	initial begin 
       $display("Begin");  
+      repeat (4) @(posedge clk) cov.a <= 3;  
+      @ (posedge clk) cov.a <= 5; 
+      repeat(3) @ (posedge clk) cov.a <= cov.a+1; 
+      repeat (2) @(posedge clk) cov.a <= 5; 
       $stop; 
     end 
   
