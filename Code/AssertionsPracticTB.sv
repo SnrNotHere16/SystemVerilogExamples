c1a0461bfef3259e593df393fabed1035789f46e
SnrNotHere16 <aquineda16@hotmail.com>
2022-01-06 11:42:16 -0800

    Create AssertionsPracticTB.sv

---
+++ Code/AssertionsPracticTB.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/19/2021 01:01:30 PM
+// Design Name: 
+// Module Name: AssertionsPracticTB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AssertionsPracticTB(
+
+    );
+endmodule
