b7d09b4da472e4ebd07b04ffee6ac2185dfa7b3c
SnrNotHere16 <aquineda16@hotmail.com>
2022-03-31 20:46:52 -0700

    Update VirtualSequence.sv

--- Code/SystemVerilogQuestions/VirtualSequence.sv
+++ Code/SystemVerilogQuestions/VirtualSequence.sv
@@ -15,3 +15,18 @@ task run_phase (uvm_phase phase);
 endtask: run_phase
 /*>vsequence is virtual sequence , v_sqr is handle of virtual sequencer, 
 vseq is started on v_sqr*/
+
+task run_phase (uvm_phase phase);
+	/// Create the Virtual Sequence
+	my_vseq vsequence = my_vseq::type_id::create(“vsequence “);
+	phase.raise_objection(this);
+	/// Virtual Sequence Initialization
+	init_vsequence (vsequence);
+	/// Start the Virtual Sequence
+	vsequence.start(null);
+	phase.drop_objection(this);
+endtask: run_phase
+/*> my_vseq is a  virtual sequence , inside init_vseq is function which has 
+connection of   the sequencer handles to the handles in virtual sequence class
+
+>vsequence.start(null) is way to start virtual sequence*/
